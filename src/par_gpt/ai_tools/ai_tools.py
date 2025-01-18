3fe891b7b8477ae248d82b1886beb4496c8e9c77
Paul Robello <probello@gmail.com>
2025-01-17 16:52:08 -0800

    add blurb on execute_code tool description asking llm to use prints to get output

--- src/par_gpt/ai_tools/ai_tools.py
+++ src/par_gpt/ai_tools/ai_tools.py
@@ -617,6 +617,7 @@ def ai_fetch_hacker_news(max_items: int = 5) -> str:
 def execute_code(code: str) -> ExecuteCommandResult:
     """
     Executes the given python code in a sandbox and returns the output.
+    You must use print statements to get the output.
     Do not assume the output is shown to the user, you must use or show it to the user in the desired format.
 
     Args:
